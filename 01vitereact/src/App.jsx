510b3485050854e98f6e5ce4455f0661554b97ad
karmveer7 <mybestdhoni@gmail.com>
2024-02-29 12:56:40 +0530

    start of react

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,10 @@
+import Chai from './chai'
+function App() {
+
+  return (
+  // <h1>Chai aur React with vite | karmveer</h1>
+  <Chai/>
+  )
+}
+
+export default App
---
+++ 01vitereact/src/chai.jsx
@@ -0,0 +1,8 @@
+function Chai(){
+    return(
+        <><h2>Honi ko kar de anhoni anhoni ko honi hot and handsome Mahendra Singh Dhoni</h2>
+        <p>mera dil bhi kitna paagal hai dhoni pr</p></>
+
+    )
+}
+export default Chai
